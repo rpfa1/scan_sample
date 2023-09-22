8694581728b77cedca4be9c81af0f28616750186
rpfa1 <141751150+rpfa1@users.noreply.github.com>
2023-09-22 17:34:33 +0100

    Update and rename test.py to code/badcode.py
    Moved and renamed the test.py file.

---
+++ code/badcode.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+
+def main():
+	secret_token="password"
+
+	bd_user = input("Enter username:")
+
+	num1 = input("Enter first number:")
+	num2 = input("Enter second number:")
+
+
+	print("You number is " + num1+num2)
+
+
+main()
+
+
